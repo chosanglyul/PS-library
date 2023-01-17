fd4efe379f659d206e66ebbfac6da9501b0efc4d
chosanglyul <chosanglyul@gmail.com>
2023-01-18 02:40:52 +0900

    Update segtree dynamic size_t to unsigned int

--- lib/segtree/segtree_dynamic.hpp
+++ lib/segtree/segtree_dynamic.hpp
@@ -8,22 +8,22 @@ template <typename node_seg, typename node_query = node_seg, typename index_t = 
 class SegtreeDynamic {
     private:
     const index_t n;
-    static const size_t MAX = __UINT32_MAX__;
+    static const unsigned int MAX = __UINT32_MAX__;
     struct Node {
-        size_t l, r;
+        unsigned int l, r;
         node_seg val = node_seg::inf();
         static Node inf() { return {MAX, MAX, node_seg::inf()}; }
     };
     vector<Node> seg;
 
-    void make(size_t &i) {
+    void make(unsigned int &i) {
         if(i == MAX) {
             i = seg.size();
             seg.push_back(Node::inf());
         }
     }
 
-    void update(const size_t i, const index_t s, const index_t e, const index_t j, const node_query &x) {
+    void update(const unsigned int i, const index_t s, const index_t e, const index_t j, const node_query &x) {
         if(s+1 == e) seg[i].val += x;
         else {
             int mi = s+e>>1;
@@ -35,14 +35,14 @@ class SegtreeDynamic {
         }
     }
 
-    node_seg query(const size_t i, const index_t s, const index_t e, const index_t l, const index_t r) const {
+    node_seg query(const unsigned int i, const index_t s, const index_t e, const index_t l, const index_t r) const {
         if(i == MAX || r <= s || e <= l) return node_seg::inf();
         if(l <= s && e <= r) return seg[i].val;
         return query(seg[i].l, s, s+e>>1, l, r)+query(seg[i].r, s+e>>1, e, l, r);
     }
     
     public:
-    SegtreeDynamic(const index_t n) : n(n) { seg = {Node::inf()}; }
+    SegtreeDynamic(index_t n) : n(n) { seg = {Node::inf()}; }
     void update(const index_t j, const node_query &x) { update(0, 0, n, j, x); }
     node_seg query(const index_t l, const index_t r) const { return query(0, 0, n, l, r); }
 };
